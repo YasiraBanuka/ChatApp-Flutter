5929fe335f48802dae8fba605bf82d2ea7eed6fb
IT21378270 <it21378270@my.sliit.lk>
2023-09-08 09:12:20 +0530

    chat page done & firebase connected

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:messaging_app/pages/chatpage.dart';
 import 'package:messaging_app/pages/home.dart';
 import 'package:messaging_app/pages/signin.dart';
 import 'package:messaging_app/pages/signup.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -35,7 +39,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: Home(),
+      home: ChatPage(),
     );
   }
 }
